a27b91153a02d651b524e4234e1247541a30376c
mdnaim346 <mdnaimreza66@gmail.com>
2026-06-10 00:09:50 +0600

    basic structure added

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'AI Copilot',
+    'version' : '1.0',
+    'sequence': 10,
+    'depends': ['web'],
+    'data': [
+        'security/ir.model.access.csv',
+        # 'views/calculator_views.xml',
+        
+    ],
+    
+    'installable': True,
+    'application': True,
+   
+    'assets': {
+       
+        'web.assets_backend': [
+           
+        ],
+        'web.assets_frontend': [
+           
+        ],
+        
+    },
+    
+}
