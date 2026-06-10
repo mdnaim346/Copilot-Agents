127e05b63a2ed8f90911df64caf0c747e4d45378
mdnaim346 <mdnaimreza66@gmail.com>
2026-06-10 23:37:30 +0600

    basic ai type response done

--- ai_service/app/main.py
+++ ai_service/app/main.py
@@ -19,6 +19,29 @@ def  home():
 def chat(request: ChatRequest):
     user_message = request.message
 
+    if "customer" in user_message:
+        return {
+            "response":"you really want to know about the customer . in next message i send you customer details"
+        }
+    elif "product" in user_message:
+        return {
+         "response":"you really want to know about the product details data. in next contact i send you details about those products"
+        }
+    elif "invoice" in user_message:
+        return {
+            "response": "You asked for invoice information. Next we will connect this to Odoo invoices."
+        }
+
+    elif "sale" in user_message or "sales" in user_message:
+        return {
+            "response": "You asked for sales information. Next we will connect this to Odoo sales orders."
+        }
+
+    else:
+        return {
+            "response": "I received your message, but I do not understand the ERP command yet."
+        }
+
     return {
         "response": f"FastAPI received your message: {user_message}"
     }
